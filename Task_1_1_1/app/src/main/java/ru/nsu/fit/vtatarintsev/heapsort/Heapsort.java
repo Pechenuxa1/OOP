f6d575b4500c9a5f37041eed8a610f23fb1ae3c2
pechenuxa1 <v.tatarintsev@g.nsu.ru>
2022-09-25 21:42:36 +0700

    Fixed bugs

--- Task_1_1_1/app/src/main/java/ru/nsu/fit/vtatarintsev/heapsort/Heapsort.java
+++ Task_1_1_1/app/src/main/java/ru/nsu/fit/vtatarintsev/heapsort/Heapsort.java
@@ -23,7 +23,7 @@ public class Heapsort {
     for (i = 0; i < array.length; i++) {
       v1 = i;
       v2 = (i - 1) / 2;
-      Utils.siftUp(array);
+      siftUp(array);
     }
     n = array.length;
     while (n > 1) {
@@ -31,8 +31,52 @@ public class Heapsort {
       v2 = n - 1;
       Utils.swap(array);
       n -= 1;
-      Utils.siftDown(array);
+      siftDown(array);
     }
 
+  }
+
+  /**
+   * siftDown finds the largest element starting from the root.
+   *
+   * @param heap is array.
+   */
+  private static void siftDown(int[] heap) {
+    if (((v1 * 2) + 2) < n) {
+      if ((heap[v1] < heap[(v1 * 2) + 1]) || (heap[v1] < heap[(v1 * 2) + 2])) {
+        if (heap[(v1 * 2) + 1] >= heap[(v1 * 2) + 2]) {
+          v2 = (v1 * 2) + 1;
+          Utils.swap(heap);
+          v1 = v2;
+          siftDown(heap);
+        } else if (heap[(v1 * 2) + 1] < heap[(v1 * 2) + 2]) {
+          v2 = (v1 * 2) + 2;
+          Utils.swap(heap);
+          v1 = v2;
+          siftDown(heap);
+        }
+      }
+    } else if (((v1 * 2) + 2) == n) {
+      if ((heap[v1] < heap[(v1 * 2) + 1])) {
+        v2 = (v1 * 2) + 1;
+        Utils.swap(heap);
+        v1 = v2;
+        siftDown(heap);
+      }
+    }
+  }
+
+  /**
+   * siftUp puts the largest element of the array at the root.
+   *
+   * @param heap is array.
+   */
+  private static void siftUp(int[] heap) {
+    if (heap[v1] > heap[v2] && v1 != 0) {
+      Utils.swap(heap);
+      v1 = v2;
+      v2 = (v2 - 1) / 2;
+      siftUp(heap);
+    }
   }
 }
