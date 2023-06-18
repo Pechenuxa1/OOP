c4de2134f980fd94a9d22d786f9e7680ed367be1
Pechenuxa1 <v.tatarintsev@g.nsu.ru>
2023-06-19 03:38:25 +0700

    add Task_2_3_1

---
+++ Task_2_3_1/lib/src/main/java/ru/nsu/fit/vtatarintsev/snake/Launcher.java
@@ -0,0 +1,10 @@
+package ru.nsu.fit.vtatarintsev.snake;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+  public static void main(String[] args) {
+    Application.launch(Main.class, args);
+  }
+}
